a2c9f09445e618264d809b646498190dba3fb99e
Vasiliy Krupka <vasiliy.k@payop.com>
2020-08-20 04:40:11 +0300

    modern 2.4 - password generate

--- [modernAngularBootcamp]/ang02-passwordGenerate/src/app/app.component.ts
+++ [modernAngularBootcamp]/ang02-passwordGenerate/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang02-passwordGenerate';
+  onButtonClick(): void {
+    console.log('Button was clicked');
+  }
 }
