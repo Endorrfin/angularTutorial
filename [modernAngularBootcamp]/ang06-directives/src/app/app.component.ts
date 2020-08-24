3b2c878f1ebd25f087890f4622663086dc1f31e8
Vasiliy Krupka <vasiliy.k@payop.com>
2020-08-24 16:21:45 +0300

    modern 6.11 directives

--- [modernAngularBootcamp]/ang06-directives/src/app/app.component.ts
+++ [modernAngularBootcamp]/ang06-directives/src/app/app.component.ts
@@ -24,72 +24,10 @@ export class AppComponent {
       title: 'At the Sky',
       url: 'https://images.unsplash.com/photo-1476673160081-cf065607f449?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
     },
-    {
-      title: 'At the Beach',
-      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-      title: 'At the See',
-      url: 'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-      title: 'At the grit',
-      url: 'https://images.unsplash.com/photo-1504321946642-8f661bf96ff0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-      title: 'At the Sky',
-      url: 'https://images.unsplash.com/photo-1476673160081-cf065607f449?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-      title: 'At the Beach',
-      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-      title: 'At the See',
-      url: 'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-      title: 'At the grit',
-      url: 'https://images.unsplash.com/photo-1504321946642-8f661bf96ff0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-      title: 'At the Sky',
-      url: 'https://images.unsplash.com/photo-1476673160081-cf065607f449?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-      title: 'At the Beach',
-      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-      title: 'At the See',
-      url: 'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-      title: 'At the grit',
-      url: 'https://images.unsplash.com/photo-1504321946642-8f661bf96ff0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-    },
-    {
-      title: 'At the Sky',
-      url: 'https://images.unsplash.com/photo-1476673160081-cf065607f449?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-    }
   ];
 
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage + index) < 5;
+  }
 
-  // getClass() {
-  //   const classes = [];
-  //
-  //   if (something()) {
-  //     classes.push('active');
-  //   }
-  //
-  //   if (comethingElse()) {
-  //     classes.push('something');
-  //   }
-  //
-  //   if (yetAnotherCheck()) {
-  //     classes.push('check');
-  //   }
-  //
-  //   return classes;
-  // }
 }
