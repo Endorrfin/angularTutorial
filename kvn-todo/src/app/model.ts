cca8e1139d11d552502b57aaf75d41399bf38e25
Vasiliy Krupka <vasiliy.k@payop.com>
2020-10-23 03:07:15 +0300

    kvn-todo from book-example start

---
+++ kvn-todo/src/app/model.ts
@@ -0,0 +1,12 @@
+// var model = {
+//   user: 'KvN',
+//   items: [
+//     {action: 'Get Shoes', done: false},
+//   ]
+// }
+
+let model = { user: 'Adam',
+  items: [{ action: 'Buy Flowers', done: false }, { action: 'Get Shoes', done: false },
+    { action: 'Collect Tickets', done: true },
+    { action: 'Call Joe', done: false }]
+};
